c1757b78012c244fc489e5dcb773f5053eb9fd87
MrBlu3Sky57 <tudor.avram@icloud.com>
2022-09-09 10:24:46 -0400

    Started Program

---
+++ Tic-Tac-Toe.java
@@ -0,0 +1,41 @@
+class main {
+    static final int EMPTY = 0;
+    static final int CROSS = 1;
+    static final int CIRCLE = 2;
+
+    public static int[][] createBoard(int length) {
+        int[][] board = new int[length][length];
+        for (int r = 0; r < length; r++) {
+            for (int s = 0; s < length; s++) {
+                board[r][s] = EMPTY;
+            }
+        }
+        return board;
+    }
+
+    public static void drawCell(int value) {
+        String c = "_";
+        if (value == 1) {
+            c = "X";
+            System.out.printf("| %s "+ c);
+        } else if (value == 2) {
+            c = "O";
+            System.out.printf("| %s "+ c);
+        }
+    }
+
+    public static void drawBoard(int[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                drawCell(board[i][j]);
+            }
+        }
+    }
+
+    public static int placeMove(int[][] board, int xCoordinate) {
+        return 1;
+    }
+    public static void main(String[] args) {
+
+    }
+}
